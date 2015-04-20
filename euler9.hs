e85937e0001dfe1b41e81482cca23a984917c6be
Michel Tabari <tabari@kth.se>
2015-04-20 17:18:21 +0200

    Create euler9.hs

---
+++ euler9.hs
@@ -0,0 +1,5 @@
+--Simple (but rather slow, takes almost 30 sec) Haskell code that solves problem 9
+
+pythagoranTriplet = [(a, b, c) | c<-[1..1000], b<-[1..c], a<-[1..b], a^2 + b^2 == c^2, a+b+c == 1000]
+
+--this returns a tuple (200, 375, 425)
