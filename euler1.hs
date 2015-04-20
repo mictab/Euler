8d06ffc4ac5e12d19d0991f065446c25670441d4
Michel Tabari <tabari@kth.se>
2015-04-20 15:41:16 +0200

    Update euler1.hs

--- euler1.hs
+++ euler1.hs
@@ -1,5 +1,5 @@
 -- Solving problem 1 using Haskell
 
-summan = sum [x | x <-[1..999], x `mod`5 == 0 || x `mod` 3 == 0]
+summan = sum [x | x <-[1..999], x `mod` 5 == 0 || x `mod` 3 == 0]
 
 --then just call "summan" and it gives you 233168
