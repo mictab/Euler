ca84172238b18aef833dd093b92db759aa065c5d
Michel Tabari <tabari@kth.se>
2015-04-20 17:22:37 +0200

    Update euler9.hs

--- euler9.hs
+++ euler9.hs
@@ -2,4 +2,4 @@
 
 pythagoranTriplet = [(a, b, c) | c<-[1..1000], b<-[1..c], a<-[1..b], a^2 + b^2 == c^2, a+b+c == 1000]
 
---this returns a tuple (200, 375, 425)
+--this returns a tuple (200, 375, 425) and then you just multiply them
